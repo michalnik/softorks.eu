d2c7ec2721abaf4178e4fc111fc6a621d6b9c909
Michal Mládek <michal.mladek.78@gmail.com>
2025-01-21 22:04:26 +0100

    Initial commit

---
+++ www/README.md
@@ -0,0 +1,11 @@
+# softorks.eu | softarna.cz
+The project of my website itself.
+
+# Environment variables needed to set
+## Development
+- `DJANGO_SETTINGS_MODULE` => **www.settings**
+- `SECRET_KEY` **xxx**
+- `DEBUG` => **True**
+- `DATABASE_NAME` => **xxx**
+
+## Production
---
+++ www/www/locale.py
@@ -0,0 +1,16 @@
+from django.middleware.locale import LocaleMiddleware
+from django.utils import translation
+from django.conf import settings
+
+
+class LanguageMiddleware(LocaleMiddleware):
+    def process_request(self, request):
+        host = request.get_host().split(':')[0]
+        language_from_domain = getattr(settings, 'DOMAIN_LANGUAGE_MAP', {}).get(host, None)
+
+        if language_from_domain is not None:
+            translation.activate(language_from_domain)
+            request.LANGUAGE_CODE = language_from_domain
+        else:
+            super().process_request(request)
+
