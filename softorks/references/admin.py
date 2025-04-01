1c7ed5186737c78a3988c97db8a603ea8a5cd73a
Michal Mládek <osvc.04923031@gmail.com>
2025-04-01 11:12:52 +0200

    just add hyperlinks to admin, related to #5

--- softorks/references/admin.py
+++ softorks/references/admin.py
@@ -1,7 +1,16 @@
+from django.utils.safestring import mark_safe
 from django.contrib import admin
 from .models import Reference
 
 
 @admin.register(Reference)
 class AuthorAdmin(admin.ModelAdmin):
-    list_display = ["name", "description", "url", "source_url"]
+    list_display = ["name", "description", "get_url", "get_source_url"]
+
+    @admin.display(description="URL")
+    def get_url(self, obj):
+        return mark_safe(f"<a href=\"{obj.url}\">{obj.url}</a>")
+
+    @admin.display(description="Source URL")
+    def get_source_url(self, obj):
+        return mark_safe(f"<a href=\"{obj.source_url}\">{obj.source_url}</a>")
